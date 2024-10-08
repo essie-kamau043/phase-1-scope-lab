eec97672e505088f98e082b1259aee2db27cca1f
essie-kamau043 <essie.k004@gmail.com>
2024-10-08 23:29:00 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+customerName = customerName.toUpperCase();
+}
+
+window.bestCustomer;
+function setBestCustomer(){
+    window.bestCustomer= 'not bob';
+}
+function overwriteBestCustomer(){
+    window.bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Kamau';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Esther'
+}
+
+
+ 
+
